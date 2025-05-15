dbaa20768f49f8a330d35fd2698b7a3252d4b7b9
Weronika Materkowska <w.m.materkowska@gmail.com>
2025-05-16 00:08:49 +0200

    add WelcomeView

--- views/WelcomeView.tsx
+++ views/WelcomeView.tsx
@@ -1,46 +1,42 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Button, Text } from 'react-native-paper';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import RootStackParamList from '../navigation/RootNavigator';
 
-import { useQuery } from '@realm/react';
-import { Item } from '../database/models/Item';
 import { Strings } from '../constants';
-
-import WelcomeView from './WelcomeView';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
 
 export default function HomeView({ navigation }: Props) {
 
-  const items = useQuery(Item);
-
-  if (items.length !== 0) {
-    return <WelcomeView />
-  }
-
   return (
     <View style={{ flex: 1, justifyContent: 'center', padding: 24 }}>
-      <Text variant="headlineMedium" style={{ textAlign: 'center', marginBottom: 32 }}>
+      <Text variant="headlineLarge" style={styles.appName}>
         {Strings.appName}
+      </Text>
+      <Text variant="titleLarge" style={styles.welcome}>
+        {Strings.welcome}
       </Text>
 
       <Button
-        mode="contained"
+        mode="outlined"
         onPress={() => {}}
         style={{ marginBottom: 16 }}
       >
         Add New Piece
       </Button>
-
-      <Button
-        mode="contained"
-        onPress={() => navigation.navigate('Wardrobe')}
-      >
-        View Wardrobe
-      </Button>
-
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  appName: {
+    textAlign: 'center',
+    marginBottom: 32
+  },
+  welcome: {
+    textAlign: 'left',
+    margin: 32,
+  }
+})
