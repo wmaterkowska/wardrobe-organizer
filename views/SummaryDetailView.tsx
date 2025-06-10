19ce20e5f768161458ce4584f2c267e0ad92fa21
Weronika Materkowska <w.m.materkowska@gmail.com>
2025-06-10 22:13:16 +0200

    add SummaryDetailView and move category summary to new component

---
+++ views/SummaryDetailView.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Realm from 'realm';
+import { useQuery } from '@realm/react';
+import { RouteProp } from '@react-navigation/native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../navigation/RootNavigator';
+
+import { View, Text } from 'react-native';
+import CategorySummary from '../components/summaries/CategorySummary';
+
+import { Item } from './../database/models/Item';
+
+type Props = NativeStackScreenProps<RootStackParamList, 'SummaryDetail'>;
+
+export default function SummaryDetailView({ route }: Props) {
+  const { type } = route.params;
+
+  const itemsKeep = useQuery(Item).filtered('want == $0', 'Keep');
+  const itemsLetGo = useQuery(Item).filtered('want == $0', 'Let go');
+
+  if (!itemsKeep.length && !itemsLetGo) {
+    return (
+      <View>
+        <Text>No items found. Add your first piece!</Text>
+      </View>
+    )
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+      {type === 'category' && (
+        <CategorySummary itemsKeep={itemsKeep} itemsLetGo={itemsLetGo} />
+      )}
+      {/* Add more summary types later */}
+    </View>
+  );
+}
